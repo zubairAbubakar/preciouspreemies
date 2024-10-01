e7aa189b901de39310fc1a85ac85b323aea70fcb
Zubair Abubakar <zuby52@gmail.com>
2024-10-01 07:42:02 +0100

    [PREEMIE-24] dockerized store app

--- actions/getCategories.tsx
+++ actions/getCategories.tsx
@@ -1,6 +1,6 @@
 import { Category } from '@/types';
 
-const CATEGORIES_URL = `${process.env.STORE_PUBLIC_URL}/categories`;
+const CATEGORIES_URL = process.env.STORE_API_URL + '/categories';
 
 const getCategories = async (): Promise<Category[]> => {
   const response = await fetch(CATEGORIES_URL);
--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'standalone',
 
-module.exports = nextConfig
+  // serverRuntimeConfig: {
+  //   apiUrl: process.env.NEXT_PUBLIC_STORE_API_URL,
+  // },
+
+  // publicRuntimeConfig: {
+  //   apiUrl: process.env.NEXT_PUBLIC_STORE_API_URL,
+  // },
+  // env: {
+  //   storeUrl:
+  //     'http://172.20.10.2:3000/api/ebf02b0d-3241-432a-a028-ea242dbf1d68',
+  // },
+};
+
+module.exports = nextConfig;
