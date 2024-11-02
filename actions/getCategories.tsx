c269f0d39832b0655458c278e2ae4c4b4ef1b340
Zubair Abubakar <zuby52@gmail.com>
2024-11-02 18:38:48 +0000

    [PREEMIE-24] added docker compose config

--- actions/getCategories.tsx
+++ actions/getCategories.tsx
@@ -5,7 +5,6 @@ const CATEGORIES_URL = process.env.STORE_API_URL + '/categories';
 const getCategories = async (): Promise<Category[]> => {
   const response = await fetch(CATEGORIES_URL);
 
-  console.log(response.json);
   return response.json();
 };
 
--- app/(routes)/page.tsx
+++
@@ -1,5 +0,0 @@
-const HomePage = () => {
-  return <div> Hello Store </div>;
-};
-
-export default HomePage;
